aca33af6a733865f61d91c73f2cabfee13adf8ba
dsyrstad <dsyrstad@1131db2a-1c84-11de-a027-6f5b8e30e268>
2008-08-09 20:52:22 +0000

    Continue work on BeanForm changes.

--- wicketwebbeans/src/main/java/net/sourceforge/wicketwebbeans/containers/BeanForm.js
+++ wicketwebbeans/src/main/java/net/sourceforge/wicketwebbeans/containers/BeanForm.js
@@ -1,85 +1,89 @@
-var wwbBeanForm = new Object();
- 
-wwbBeanForm.indicatorOn = function() {
-    var element = document.getElementById("beanFormIndicatorOn");
-    if (element) element.style.display = "block";
-    element = document.getElementById("beanFormIndicatorError");
-    if (element) element.style.display = "none";
-    element = document.getElementById("beanFormIndicatorOff");
-    if (element) element.style.display = "none";
-}
+var wwbBeanForm =  {
+    indicatorOn: function() {
+        var element = document.getElementById("beanFormIndicatorOn");
+        if (element) element.style.display = "block";
+        element = document.getElementById("beanFormIndicatorError");
+        if (element) element.style.display = "none";
+        element = document.getElementById("beanFormIndicatorOff");
+        if (element) element.style.display = "none";
+    },
+    
+    indicatorOff: function() {
+        var element = document.getElementById("beanFormIndicatorOff");
+        if (element) element.style.display = "block";
+        element = document.getElementById("beanFormIndicatorOn");
+        if (element) element.style.display = "none";
+        element = document.getElementById("beanFormIndicatorError");
+        if (element) element.style.display = "none";
+        wwbBeanForm.refocus();
+    },
+     
+    indicatorError: function() {
+        element = document.getElementById("beanFormIndicatorOn");
+        if (element) element.style.display = "none";
+        element = document.getElementById("beanFormIndicatorOff");
+        if (element) element.style.display = "block";
+    },
 
-wwbBeanForm.indicatorOff = function() {
-    var element = document.getElementById("beanFormIndicatorOff");
-    if (element) element.style.display = "block";
-    element = document.getElementById("beanFormIndicatorOn");
-    if (element) element.style.display = "none";
-    element = document.getElementById("beanFormIndicatorError");
-    if (element) element.style.display = "none";
-    wwbBeanForm.refocus();
-}
- 
-wwbBeanForm.indicatorError = function() {
-    element = document.getElementById("beanFormIndicatorOn");
-    if (element) element.style.display = "none";
-    element = document.getElementById("beanFormIndicatorOff");
-    if (element) element.style.display = "block";
-}
+    // Track field with last focus in case we're refreshed.
+    onFocus: function(field) {
+        var element = document.getElementById("bfFocusField");
+        if (element) {
+            element.value = field.id;
+        }
+    },
+     
+    onChange: function(field) {
+        alert("changed " + field.id);
+    },
 
-// Track field with last focus in case we're refreshed.
-wwbBeanForm.onFocus() = function(field) {
-    var element = document.getElementById("bfFocusField");
-    if (element) {
-        element.value = field.id;
-    }
-}
- 
-wwbBeanForm.refocus = function() {
-    var element = document.getElementById("bfFocusField");
-    if (element) {
-        var field = element.value ? document.getElementById(element.value) : null;
-        if (field) {
-            field.focus();
+    refocus: function() {
+        var element = document.getElementById("bfFocusField");
+        if (element) {
+            var field = element.value ? document.getElementById(element.value) : null;
+            if (field) {
+                field.focus();
+            }
+            else {
+                bfFocusFirst(element.form, false);
+            }
+        }
+    },
+     
+    focusFirst: function(form, focusFirstEmpty) {
+        for (var i = 0; i < form.elements.length; i++ ) {
+            if (form.elements[ i ].type != "hidden" &&
+                !form.elements[ i ].disabled &&
+                !form.elements[ i ].readOnly &&
+                (!focusFirstEmpty || form.elements[i].value == "")) {
+                var field = form.elements[ i ];
+                field.focus();
+                var element = document.getElementById("bfFocusField");
+                if (element) {
+                    element.value = field.id;
+                }
+                break;
+            }
+        }
+    },
+     
+    handleGlobalKeyEvent: function(event) {
+        var keyCode;
+        if (event.which) {
+            keyCode = event.which; // Mozilla
         }
         else {
-            bfFocusFirst(element.form, false);
+            keyCode = event.keyCode; // IE
         }
-    }
-}
- 
-wwbBeanForm.focusFirst = function(form, focusFirstEmpty) {
-    for (var i = 0; i < form.elements.length; i++ ) {
-        if (form.elements[ i ].type != "hidden" &&
-            !form.elements[ i ].disabled &&
-            !form.elements[ i ].readOnly &&
-            (!focusFirstEmpty || form.elements[i].value == "")) {
-            var field = form.elements[ i ];
-            field.focus();
-            var element = document.getElementById("bfFocusField");
+     
+        if (keyCode == 13) {
+            var element = document.getElementById("bfDefaultButton");
             if (element) {
-                element.value = field.id;
+                element.onclick();
+                return false;
             }
-            break;
         }
+        
+        return true;
     }
-}
- 
-wwbBeanForm.handleGlobalKeyEvent(event) {
-    var keyCode;
-    if (event.which) {
-        keyCode = event.which; // Mozilla
-    }
-    else {
-        keyCode = event.keyCode; // IE
-    }
- 
-    if (keyCode == 13) {
-        var element = document.getElementById("bfDefaultButton");
-        if (element) {
-            element.onclick();
-            return false;
-        }
-    }
-    
-    return true;
-}
+};
